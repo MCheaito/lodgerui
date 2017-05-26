4969ead435dc98f3e73a122f86f193b183ff38b8
Mcheaito@hplaptop <Mouhamad.cheaito@gmail.com>
2017-05-26 00:56:05 -0400

    completer le formulaire booking

--- src/app/booking/booking.component.ts
+++ src/app/booking/booking.component.ts
@@ -10,13 +10,26 @@ import {Guid} from '../utils/Guid';
 })
 
 export class BookingComponent implements OnInit {
-booking: Booking ;
+booking: Booking;
 
 
   constructor() { 
     this.booking = new Booking();
     this.booking.title='sddddddddddddd';
     this.booking.bookingGuid = Guid.newGuid();
+    this.booking.type="P";
+    this.booking.title ="";
+    this.booking.roomCount=1,
+    this.booking.adultCount=1 ,
+    this.booking.childrenCount=0;
+    this.booking.promotionCode="";
+    this.booking.companyName="";
+    this.booking.remarks="";
+    this.booking.referenceNumber="";
+    this.booking.email="";
+    this.booking.phoneNumber="";
+    this.booking.typeOfLocation="" ;
+
 
     
   }
