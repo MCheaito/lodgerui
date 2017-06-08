a4d5162eb0a8b6d7d91f8c180c5280343d61f994
Mcheaito@hplaptop <Mouhamad.cheaito@gmail.com>
2017-06-08 02:33:23 -0400

    Booking.component : add validation infrastructure

--- src/app/booking/booking.component.ts
+++ src/app/booking/booking.component.ts
@@ -1,5 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, AfterViewChecked, ViewChild } from '@angular/core';
 import {NgForm} from '@angular/forms';
+//todo ajouter les validatore 
 import { Router, ActivatedRoute, Params } from '@angular/router';
 import 'rxjs/add/operator/switchMap';
 
@@ -18,34 +19,80 @@ promotionList:Promotions[];
 bookingTypesList: BookingTypes[];
 messages:string[];
 booking: Booking;
+
+ bookingForm: NgForm;
+  @ViewChild('bookingForm') currentForm: NgForm;
+
   constructor(
- 
       private route: ActivatedRoute,
       private router: Router,
-      private _service:BookingService) { 
-        this.booking = new Booking();
-  this.route.params
+      private _service:BookingService) 
+  { 
+
+    this.booking = new Booking();
+    /*
+    this.route.params
     //(+) converts string 'id' to a number
     .switchMap((params: Params) => this._service.getBooking(params['id']))
     .subscribe((booking: Booking) => this.booking = booking);
-
-     }
+*/
+   }
 
   ngOnInit() {
+    /*
      this._service.getListOfPromotions()
      .then(promo => this.promotionList= promo);
 
      this._service.getListOfBookingTypes()
-     .then(types=>this.bookingTypesList = types);
+     .then(types=>this.bookingTypesList = types);*/
 
   } 
 
   submitForm(myForm:NgForm) {
- //alert(JSON.stringify(myForm.value));
- //alert(JSON.stringify(this.booking));
-//alert(JSON.stringify(this.promotionList));
-
-// this.booking.remarks=JSON.stringify(this.booking);
+    // this.booking.remarks=JSON.stringify(this.booking);
  }
+
+   ngAfterViewChecked() {
+    this.formChanged();
+  }
+
+  formChanged() {
+    if (this.currentForm === this.bookingForm) { return; }
+    this.bookingForm = this.currentForm;
+    if (this.bookingForm) {
+      this.bookingForm.valueChanges
+        .subscribe(data => this.onValueChanged(data));
+    }
+  }
+
+  onValueChanged(data?: any) {
+    if (!this.bookingForm) { return; }
+    const form = this.bookingForm.form;
+
+    for (const field in this.formErrors) {
+      // clear previous error message (if any)
+      this.formErrors[field] = '';
+      const control = form.get(field);
+
+      if (control && control.dirty && !control.valid) {
+        const messages = this.validationMessages[field];
+        for (const key in control.errors) {
+          this.formErrors[field] += messages[key] + ' ';
+        }
+      }
+    }
+  }
+
+  formErrors = {
+    'bookingTitle': ''
+  };
+
+  validationMessages = {
+    'bookingTitle': {
+      'required':      'Name is required.',
+      'minlength':     'Name must be at least 4 characters long.',
+      'maxlength':     'Name cannot be more than 24 characters long.'
+    }
+  };
 
 }
